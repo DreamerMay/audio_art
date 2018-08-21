919b1f3b507063459c5be41e1ea86f2daffc8bf4
Sherine <sherinemay79@gmail.com>
2018-08-21 21:45:40 +1000

    re-arrange code and add variable declearation

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ console.log("Audio_art");
 // define audio context
 // create sound source
 const audioCtx = new(window.AudioContext || window.wekitAudioContext)();
-
+const analyser = audioCtx.createAnalyser();
+analyser.minDecibels = -90;
+analyser.maxDecibels = -10;
+analyser.smoothingTimeConstant = 0.85;
 
 //Activate Mic with user permission
 window.onload = function () {
@@ -13,48 +16,53 @@ window.onload = function () {
   navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
     const audio = document.querySelector('audio');
     audio.srcObject = mediaStream;
-    //audio.play();
-  }).catch(function(err){
+    // audio.play();
+    source.connect(analyser);
+    analyser.connect(distortion);
+    distortion.connect(audioCtx.destination);
+    draw ()
+  }).catch(function(err) {
     console.log("Web Audio API not support");
   })
+  function analyserAudio(stream) {
+    source = audioCtx.createMediaStreamSource(stream); // connect source to destination.
+  }
 }
 
-// Detect Mic audio
-// extract data from audio source, and node connected to your audio source at same point between your source and destination
-const analyser = audioCtx.createAnalyser();
-source = audioCtx.createMediaStreamSource(Stream); // connect source to destination.
-source.connect(analyser);
-analyser.connect(distortion);
-distortion.connect(audioCtx.destination);
 
-//Collecting data from the audio source
 analyser.fftSize = 2048;
 const bufferLength = analyser.frequencyBinCount; // frequency data for 32-bit floating point numbers.
-const dataArray = new Unit8Array(bufferLength); // Unit8Array for TimeDomainData array
+console.log(bufferLength);
+const dataArray = new Uint8Array(bufferLength); // Unit8Array for TimeDomainData array
+console.log(dataArray);
 
-// detect BMP from audio
+// TODO detect BMP from audio
 
+const canvas = document.getElementById('canvas');
+const canvasCtx = canvas.getContext('2d');
 
-// clear canvas
+WIDTH = canvas.width;
+HEIGHT = canvas.height;
 canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
 function draw () {
   const drawVisual = requestAnimationFrame(draw); // loop drawing function once started.
   //Retreive data and copy into array
   analyser.getByteTimeDomainData(dataArray);
+  console.log(dataArray);
   canvasCtx.fillStyle = 'rgb(200, 200, 200)'; // fill canvas
   canvasCtx.fillRect(0, 0, WIDTH, HEIGH);
 
   // Set a line width and stroke colour for the wave we will draw, then begin drawing a path
   canvasCtx.lineWidth = 2;
   canvasCtx.strokeStyle = 'rbg(0,0,0)';
+
   canvasCtx.beginPath(); // begin drawing path
 
   //determin width of each segment of the line to be draw
   const sliceWidth = WIDTH * 1.0 / bufferLength;
   const x = 0;
 
-  //run through loop
   for (var i = 0; i < bufferLength; i++) {
     const v = dataArray[i] / 128.0;
     const y = v * HEIGHT / 2;
@@ -64,10 +72,8 @@ function draw () {
     } else {
       canvasCtx.lineTo(x, y);
     }
-    x += slideWidth;
+    x += sliceWidth;
   }
   canvasCtx.lineTo(canvas.width, canvas.height/2);
-    canvasCtx.stroke();
+  canvasCtx.stroke();
 };
-
-draw();
