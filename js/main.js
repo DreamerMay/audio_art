5e3257b214b1aba59d0c9aa4e06315c61e35bb8a
Sherine <sherinemay79@gmail.com>
2018-08-26 07:41:20 +1000

    added BPM controller but biquadfilter giving zero data

--- js/main.js
+++ js/main.js
@@ -10,10 +10,9 @@ analyser.minDecibels = -90;
 analyser.maxDecibels = -10;
 analyser.smoothingTimeConstant = 0.85;
 
-
-var distortion = audioCtx.createWaveShaper();
-var gainNode = audioCtx.createGain();
-var biquadFilter = audioCtx.createBiquadFilter();
+let distortion = audioCtx.createWaveShaper();
+let gainNode = audioCtx.createGain();
+let biquadFilter = audioCtx.createBiquadFilter(); // simple low-order filer
 // need to add to remove echo or direct source to correct audio path
 // source = audioCtx.createMediaStreamSource(stream);
 // source.connect(analyser);
@@ -21,6 +20,10 @@ var biquadFilter = audioCtx.createBiquadFilter();
 // distortion.connect(audioCtx.destination);
 
 let source;
+
+
+
+
 //================
 // USER CONTROL
 //================
@@ -37,13 +40,11 @@ $("#microphone").on('click', function () {
 
     audio.srcObject = mediaStream;
     // Detect Mic audio
-
     source.connect(analyser);
-    analyser.connect(distortion);
-    distortion.connect(biquadFilter);
+    analyser.connect(biquadFilter);
+    // distortion.connect(biquadFilter);
     biquadFilter.connect(gainNode);
     gainNode.connect(audioCtx.destination);
-
 
     draw();
 
@@ -79,7 +80,7 @@ $("#microphone").on('click', function () {
 //Creating an audio object
 const audioSrc = new Audio();
 // audioSrc.src = 'https://github.com/DreamerMay/audio_art/blob/master/assets/Love.mp3';
-audioSrc.src = 'assets/Intro.mp3';
+audioSrc.src = 'assets/Love.mp3';
 audioSrc.controls = true;
 audioSrc.autoplay = false;
 audioSrc.loop = true;
@@ -109,7 +110,6 @@ $("#start").on('click', function() {
   });
   scriptNode.onaudioprocess = function () {
     analyser.getByteTimeDomainData(dataArray);
-    getPeakAtThreshold(dataArray, threshold);
     draw();
   }
 });
@@ -161,17 +161,97 @@ function draw () {
     canvasCtx.stroke();
 };
 
-// Function to identify peaks
-function getPeakAtThreshold(data, threshold) {
-  const peaksArray = [];
-  const length = data.length;
-  for (var i = 0; i < length; ) {
-    if (data[i] > thereshold) {
-      peaksArray.push(i);
-      // Skip forward ~ 1/4s to get past this peak.
-      i += 10000;
-    }
-    i++;
+//================
+// BMP collection
+//================
+//https://stackoverflow.com/questions/30110701/how-can-i-use-js-webaudioapi-for-beat-detection/41354342
+
+//Load audio file into array and run through low pass filter
+function createBuffers(url) {
+//Fetch Audio Track via AJAX with URL
+request = new XMLHttpRequest();
+
+request.open('GET', url, true);
+request.responseType = 'arraybuffer';
+
+request.onload = function(ajaxBuffer) {
+  // create and save original buffer audio context in "originalBuffer"
+  const songLength = ajaxBuffer.total;
+
+  // Arguments: Channels, Length, Sample Rate
+  const offlineCtx = new OfflineAudioContext(1, songLength, 44100);
+  bufferSource = offlineCtx.createBufferSource();
+  const audioData = request.response;
+  console.log(audioData);
+  audioCtx.decodeAudioData(audioData, function(buffer){
+
+    window.originalBuffer = buffer.getChannelData(0);
+    let bufferSource = offlineCtx.createBufferSource();
+    bufferSource.buffer = buffer;
+
+    //Create a Low Pass Filter to isolate Low and beat
+    let lowPassFilter = offlineCtx.createBiquadFilter();
+    lowPassFilter.type = "lowpass";
+    lowPassFilter.frequency.value = 140;
+    bufferSource.connect(lowPassFilter);
+    lowPassFilter.connect(offlineCtx.destination);
+
+      // Render this lowpass filter data to new Audio context and Save it in "lowPassBuffer"
+      offlineCtx.startRendering().then(function(lowPassAudioBuffer) {
+        let song = audioCtx.createBufferSource();
+        song.buffer = lowPassAudioBuffer;
+        song.connect(audioCtx.destination);
+
+        // Save lowPassBuffer in Global Array
+        window.lowPassBuffer = song.buffer.getChannelData(0);
+        console.log("Low Pass Buffer Rendered!");
+      });
+    },
+  function(e){});
   }
-  return peaksArray
+  request.send();
+};
+
+createBuffers(audioSrc.src);
+
+window.lowPassBuffer // Low Pass Array Buffer
+window.originalBuffer // Original Non Filtered Array Buffer
+
+function getClip(length, startTime, data) {
+  let clip_length = length * 44100;
+  let section = startTime * 44100;
+  let newArr = [];
+
+  for (let i = 0; i < clip_length; i++) {
+    newArr.push(data[section + i]);
+  }
+  return newArr;
+};
+// Overwrite our array buffer to a 10 sec clip starting
+window.biquadFilter = getClip(10, 10, biquadFilter); // TODO: need fix biquadFilter was undfined data
+
+//Down Sample your clip
+function getSampleClip(data, samples) {
+  let newArray = [];
+  let modulus_coefficient = Math.round(data.length / samples);
+
+  for (let i = 0; i < data.length; i++) {
+    if (i % modulus_coefficient == 0) {
+      newArray.push(data[i]);
+    }
+  }
+  return newArray;
+};
+
+lowPassBuffer = getSampleClip(biquadFilter, 300);
+
+// Normalize Your data
+function normalizeArray(data) {
+  let newArray = [];
+  for (var i = 0; i < data.length; i++) {
+    newArray.push(Math.abs(Math.round( (data[i + 1] - data[i] * 1000) ) ) );
+  }
+  return newArray;
 }
+
+lowPassBuffer = normalizeArray(biquadFilter);
